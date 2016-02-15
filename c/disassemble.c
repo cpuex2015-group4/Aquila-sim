99ea98659be6065f1465ee905d819e51daf2bb52
Hiroki Konishi <ind.med.swim.ut.713k@gmail.com>
2016-02-16 02:50:07 +0900

    disassemble moduled

---
+++ c/disassemble.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <signal.h>
+#include "./utils.h"
+#include "./simulator.h"
+#include "./debugger.h"
+
+void disassemble(instruction inst, operands ops)
+{
+	puts("side effect");
+}
---
+++ c/disassemble.h
@@ -0,0 +1,7 @@
+#ifndef DISASSEMBER_H
+#define DISASSEMBER_H
+
+extern void disassemble(instruction, operands);
+
+#endif
+
